d00250b9eada7ab8a5379f0752441ff0e1440dfc
Axelekonj <axelekonj@gmail.com>
2020-03-03 18:22:14 +0200

    addtinyone

---
+++ tinyone/js/main.js
@@ -0,0 +1,7 @@
+$(function () {
+   $('.book').click(Function (){
+            $('#modal').addClass('.show'); 
+})
+$('.modal2__overlay').click(function (){
+    $('#modal').removeClass('.show')
+});
